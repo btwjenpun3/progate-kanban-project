d030e40e4e207bea8a3e3d2c6e3b66dc2f37518f
Helmi <muhamadkelmi@gmail.com>
2023-07-14 16:59:38 +0700

    Add create

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -75,4 +75,12 @@ class TaskController extends Controller
 
         return view('tasks.edit', ['pageTitle' => $pageTitle, 'task' => $task]);
     }
+
+    public function create()
+    {
+        $pageTitle = 'Create Task';
+        $tasks = $this->tasks;
+        
+        return view('tasks.create', ['pageTitle' => $pageTitle]);
+    }
 }
