a2964c0b8087752c3ed588cff6d4638a909f4886
Helmi <muhamadkelmi@gmail.com>
2023-08-09 19:57:41 +0700

    Develop logout

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,7 @@ Route::name('auth.')
         Route::post('signup', 'signup')->name('signup');
         Route::get('login', 'loginForm')->name('loginForm');
         Route::post('login', 'login')->name('login');
+        Route::post('logout', 'logout')->name('logout');
     });
 
 Route::get('/halo', function () {
